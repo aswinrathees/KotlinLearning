33ec5b8e98e7f973c764e41608c096d140b9f31c
Aswin Rathees <aswinrathees.tech@gmail.com>
2022-08-17 10:47:03 +0530

    CoRoutineDemoOne with suspend and withContext

--- CoRoutineDemoOne/app/src/main/java/com/opensource/coroutinedemoone/MainActivity.kt
+++ CoRoutineDemoOne/app/src/main/java/com/opensource/coroutinedemoone/MainActivity.kt
@@ -5,12 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val buttonClick: Button = findViewById(R.id.btnCount)
         val buttonDownloadUserData: Button = findViewById(R.id.btnDownloadUserData)
         val textView: TextView = findViewById(R.id.tvCount)
+        var updateTextView: TextView = findViewById(R.id.tvUserMessage)
 
         buttonClick.setOnClickListener {
             textView.text = count++.toString()
@@ -25,14 +25,18 @@ class MainActivity : AppCompatActivity() {
 
         buttonDownloadUserData.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                downloadUserData()
+                downloadUserData(updateTextView)
             }
         }
     }
 
-    private fun downloadUserData() {
-        for (i in 1..10) {
-            Log.i("CoroutineTag", "Downloading user $i in ${Thread.currentThread().name}")
+    private suspend fun downloadUserData(updateTextView: TextView) {
+        withContext(Dispatchers.Main) {
+            for (i in 1..10000) {
+                Log.i("CoroutineTag", "Downloading user $i in ${Thread.currentThread().name}")
+                updateTextView.text = "Downloading user $i in ${Thread.currentThread().name}"
+                delay(1000)
+            }
         }
     }
 }
