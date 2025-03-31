2c644d0fa6f29b506ace50debed997a1552acc83
Aswin Rathees <33830946+aswinrathees@users.noreply.github.com>
2025-03-31 16:13:29 +0530

    WorkManager OneTimeWorkRequest enhancements

--- WorkManagerDemo/app/src/main/java/com/opensource/workmanagerdemo/MainActivity.kt
+++ WorkManagerDemo/app/src/main/java/com/opensource/workmanagerdemo/MainActivity.kt
@@ -1,26 +1,49 @@
 package com.opensource.workmanagerdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.opensource.workmanagerdemo.utils.UploadWorker
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvStatus: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startButton = findViewById<Button>(R.id.button)
-        startButton.setOnClickListener {
-            setOneTimeWorkRequest()
+        findViewById<Button>(R.id.button).apply {
+            setOnClickListener {
+                setOneTimeWorkRequest()
+            }
         }
+
+        tvStatus = findViewById(R.id.textView)
     }
 
     private fun setOneTimeWorkRequest() {
-        val uploadRequest: OneTimeWorkRequest =
-            OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
-        WorkManager.getInstance(applicationContext).enqueue(uploadRequest)
+        val workManager =  WorkManager.getInstance(this)
+        val constraints =  Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val uploadWorkRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
+            .build()
+
+        //Note: WorkerChaining can be done with workManager.beginWith(worker1).then(worker2). etc .enqueue()
+        //Note: Parallel work can done with creating an mutable list of workers and adding it in beginWith()
+        //Note: Periodic work can be done with PeriodicWorkRequest.Builder() which takes an argument for repeatInterval and the TimeUnit
+
+        workManager.enqueue(uploadWorkRequest)
+        workManager.getWorkInfoByIdLiveData(uploadWorkRequest.id).observe(this) {
+            tvStatus.text = it.state.name
+        }
     }
 }
--- WorkManagerDemo/app/src/main/java/com/opensource/workmanagerdemo/utils/UploadWorker.kt
+++ WorkManagerDemo/app/src/main/java/com/opensource/workmanagerdemo/utils/UploadWorker.kt
@@ -4,20 +4,20 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import java.lang.Exception
+import kotlin.Exception
 
 class UploadWorker(
-    context: Context, parameters: WorkerParameters,
-    workerParams: WorkerParameters
+    context: Context, workerParams: WorkerParameters
 ) : Worker(context, workerParams) {
+
     override fun doWork(): Result {
-        try {
+        return try {
             for (index: Int in 1..60) {
                 Log.i("WorkManagerDemo", "Uploading $index")
             }
-            return Result.success()
-        } catch (e:Exception) {
-            return Result.failure()
+            Result.success()
+        } catch (e: Exception) {
+            Result.failure()
         }
     }
 }
